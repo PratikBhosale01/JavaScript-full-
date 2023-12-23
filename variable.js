1d6297f47bcc3dabb2213349240e019b5f1b4ec7
Pratik Bhosale <pratikbhosale0003@gmail.com>
2023-12-23 20:27:26 +0530

    done with variables

--- variable.js
+++ variable.js
@@ -1,12 +1,13 @@
-// JS is a programming language. We use it to give instructions to the computer.
-// Js is dynamic programming language/dynamically typed language
+//**  JS is a programming language. We use it to give instructions to the computer.
+//**  Js is dynamic programming language/dynamically typed language
 
 //Variables are containers for data
 
 
 
 
-//defined variables
+//***defined variables
+
 fullName = "Tony Stark";
 age = 24;
 price = 99.99 ;
@@ -21,7 +22,7 @@ isFottow = true;
 
 
 
- //rules
+ //***rules
 
 // •  Variable names are case sensitive; "a" & "A" is different.
 fullname ="rahul p";
@@ -38,3 +39,76 @@ $fullName ="tony stark";
 
 console. log (fullname);
 // • Reserved words cannot be variable names.
+
+
+//  ***let const and var 
+
+//1 - var : Variable can be re-declared & updated. A global scope variable. (used upto 2015 before  ES6)
+
+ var age = 10; 
+
+ var age = 20; //redeclaration allowed
+
+ age = 30 ; // updation allowed
+
+//2 - let: Variable cannot be re-declared but can be updated. A block scope variable.
+
+let myAge = 10; 
+
+ //let myAge = 20; //redeclaration not allowed
+
+ myAge = 30 ; // updation allowed
+
+
+//3 - const : Variable cannot be re-declared or updated. A block scope variable.
+
+const newAge = 10; 
+
+ //const newAge = 20; //redeclaration not allowed
+
+// newAge = 30 ; // updation not allowed
+
+// const obj - key value we can change 
+
+
+// *** Data types ****
+
+//1 Number 
+let num = 42;
+console.log(typeof num); // Output: "number"
+
+
+//String
+let str = "Hello, World!";
+console.log(typeof str); // Output: "string"
+
+//boolean
+let isTrue = true;
+console.log(typeof isTrue); // Output: "boolean"
+
+//undefined
+let undefinedVar;
+console.log(typeof undefinedVar); // Output: "undefined"
+
+//Null
+let nullVar = null;
+console.log(typeof nullVar); // Output: "object" (Note: typeof null is a historical bug in JavaScript)
+
+
+
+//symbol
+let sym = Symbol("unique");
+console.log(typeof sym); // Output: "symbol"
+
+
+
+//object
+
+const student={
+fullName: "Rahul Kumar" ,
+age: 20,
+cgpa:8.2,
+isPass: true,};
+
+student["age"] = student["age"] + 1;
+console.log(student ["age"] ) ;
