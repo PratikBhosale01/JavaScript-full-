8d0ad3bfa7b2f332228e13e7556bd1c2f806c1d0
Pratik Bhosale <pratikbhosale0003@gmail.com>
2023-12-23 20:41:36 +0530

    js var excercise

---
+++ variableExercise.js
@@ -0,0 +1,12 @@
+//Creating a new product object
+
+const product = {
+    productName: "Dell inspiron 15 3000",
+    rating : 3.9,
+    offer : 5,
+    price :56000,
+
+}
+
+
+console.log(product["productName"]);
