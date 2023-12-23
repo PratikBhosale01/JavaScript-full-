dfa55fcc8962f3b48f5c331220e99fb90382f647
Pratik Bhosale <pratikbhosale0003@gmail.com>
2023-12-23 14:56:39 +0530

    var rule

--- variable.js
+++ variable.js
@@ -1,4 +1,40 @@
 // JS is a programming language. We use it to give instructions to the computer.
+// Js is dynamic programming language/dynamically typed language
+
+//Variables are containers for data
+
+
+
+
+//defined variables
+fullName = "Tony Stark";
+age = 24;
+price = 99.99 ;
+radius = 14;
+a = null;
+Y = undefined;
+isFottow = true;
+
 
 // console.log is used to print 
- console.log("Hello world!");
+ console.log(age);
+
+
+
+ //rules
+
+// •  Variable names are case sensitive; "a" & "A" is different.
+fullname ="rahul p";
+fullName= "Pritam p";
+console. log ( fullname) ;
+console. log (fullName) ;
+
+// • Only letters, digits, underscore(_ ) and $ is allowed. (not even space)
+// • Only a letter, underscore( _ ) or $ should be 1st character.
+
+futtName = "tony stark";
+_fullName = "tony stark";
+$fullName ="tony stark";
+
+console. log (fullname);
+// • Reserved words cannot be variable names.
