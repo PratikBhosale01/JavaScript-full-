33970f64912d6e22b9dff5cad10212c4848f7b81
Pratik Bhosale <pratikbhosale0003@gmail.com>
2023-12-24 19:32:48 +0530

    Oprators

---
+++ operator&CondiStat.js
@@ -0,0 +1,31 @@
+//Operator And COnditoionalStatement
+
+//----------------------------------------------------------------
+//operators:-
+//Used to perform some operation on data
+// + -  / *
+// Modulus    "%"
+// Exponentiation "**"
+// Increment "++"
+// Decrement "--"
+let A = 7;
+let B = 3;
+let C = A - B;
+let D = A + B;
+let E = A * B;
+let F = A / B;
+let G = A % B;
+let H = A ** B; //A^B 
+ A++ ;
+ B-- ;
+
+
+console.log("A =", A, "& B =",B);
+console.log("A + B = ", D ); 
+console.log("A - B = ", C );
+console.log("A * B = ", E );
+console.log("A / B = ", F );
+console.log("A % B = ", G );
+console.log("A** B = ", H );
+console.log("A++ =", A );
+console.log("B-- =", B );
