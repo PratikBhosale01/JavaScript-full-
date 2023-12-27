9c428912d65e78759ee8b7e66fb84779c9ce0761
Pratik Bhosale <pratikbhosale0003@gmail.com>
2023-12-27 19:53:10 +0530

    Q1 done and attached with html

---
+++ op&conEXercise.js
@@ -0,0 +1,10 @@
+// Qsl. Get user to input a number using prompt("Enter a number;"). Check if the number is
+// a multiple of 5 or not.
+
+let num = prompt("Enter a number");
+let result =num%5
+if(result ==0){
+    console.log("number is  a multiple of 5");
+}else{
+    console.log("number is a not multiple of 5");
+}
