ff4d974dc647f7bc25409fe163ed6ecb008446a3
Pratik Bhosale <pratikbhosale0003@gmail.com>
2023-12-30 23:40:49 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,23 @@
+// -----------------------------arrays -----------------------------
+//It is nothing but collection of items 
+
+
+let heros = [ "iron man" , "spider man", "thor", "hulk"];
+let marks = [76,67,54,79];
+
+console.log(heros, marks);
+console.log(heros.length, marks.length);
+
+//array indices (index)
+
+console.log(heros[0]);
+
+console.log(marks[0]);
+marks[0] = 70; // changing value at index 0
+console.log(marks[0]);
+
+
+heros[0] ="tony stark";
+console.log(heros[0]); // array is mutable whereas string is immutable
+
+//================================================================
