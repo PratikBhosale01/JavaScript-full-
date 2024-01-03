7f8b4689a7d33d07a04141c32a641469c544e4a6
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-01-03 23:53:42 +0530

    DOM 1

---
+++ DOM1.js
@@ -0,0 +1,15 @@
+//---------------------------DOM------------------
+//window object
+//The window object represents an open window in a browser. It is browser's object (not JavaScript's) & is automatically created by browser.
+
+//It is a global object with lots of properties & methods.
+
+console.log("Hey");
+
+window.console.log("Hey windows");
+
+//What is DOM ?
+//When a web page is loaded, the browser creates a Document Object Model (DOM) of the page.
+// DOM help to provide Dynamic change or manipulation in web pages.
+
+//---------------------DOM manipulation --------------------
