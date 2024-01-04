45d65e407fdab41cbdd0239ad744a60f3a64d448
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-01-04 23:11:17 +0530

    Dom manipl

--- DOM1.js
+++ DOM1.js
@@ -13,3 +13,43 @@ window.console.log("Hey windows");
 // DOM help to provide Dynamic change or manipulation in web pages.
 
 //---------------------DOM manipulation --------------------
+// # Selecting with id
+//      document.getElementById("myId")
+
+let head = document.getElementById("head");
+
+console.dir(head);
+
+// # Selecting with class  (It returns HTML Collection)
+//    document.getElementsByClassName("myClass")
+
+let R = document.getElementsByClassName("R");
+console.dir(R);
+
+// # Selecting with tag
+//      document.getElementsByTagName("p")
+
+let tag = document.getElementsByTagName("p");
+console.dir(tag);
+
+// Query Selector
+// document.querySeIector("myId / myClass / tag")
+// returns first element
+
+let firstEleId = document.querySelector("#head")  //BY id
+console.dir(firstEleId);
+
+let firstEleClass = document.querySelector(".R") //BY class
+console.dir(firstEleClass);
+
+let firstEle = document.querySelector("p")  //By tag
+console.dir(firstEle);
+
+
+// document.querySelectorAll("myld / myClass / tag")
+// returns a NodeList
+let allEleClass = document.querySelectorAll(".R"); //By class
+console.dir(allEleClass); 
+
+let allEle = document.querySelectorAll("p") //By tag
+console.dir(allEle);
