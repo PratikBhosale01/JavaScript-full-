e95282e315d0915b056dc7f09c18e01bd3d2b69f
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-01-17 21:35:48 +0530

    Promise

--- callback.js
+++ callback.js
@@ -43,3 +43,27 @@ calculator( 1 , 2 ,sum); // callback function pass with only name without perent
 // Callback Hell : Nested callbacks stacked below one another forming a pyramid structure.
 // (Pyramid of Doom)
 // This style of programming becomes difficult to understand & manage.
+
+function getData(dataId,getNextData  ){
+   setTimeout(()=>{
+    console.log("data", dataId);
+    if(getNextData){
+    getNextData();
+  }
+   },2000);
+}
+// callback Hell 
+getData(1,  ()=>{
+  getData(2, ()=>{
+    getData(3, ()=>{
+      getData(4);
+    });
+  });
+});
+
+
+//Promises
+//Promise is for "eventual" completion of task. It is an object in JS.
+// It is a solution to callback hell.
+// promise state are pending , fulfilled [resolve], rejected
+//*resolve & reject are callbacks provided by JS
