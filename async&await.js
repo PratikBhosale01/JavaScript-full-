8f42d6d5ff51b254f444be42bc22c76341bec3ae
Pratik Bhosale <pratikbhosale0003@gmail.com>
2024-01-20 20:36:19 +0530

    IIFE

--- async&await.js
+++ async&await.js
@@ -19,3 +19,41 @@ async function weather(){
 }
 
 weather();
+
+////////////////////\
+
+
+function getData(dataId){
+    return new Promise ((resolve, reject)  =>{
+     setTimeout(()=>{
+        console.log("dataId", dataId);
+        resolve("success");
+    }, 2000);
+    });
+}
+
+// async function getAllData(){
+//     await getData(1);
+//     await getData(2);
+//     await getData(3);
+//     await getData(4);
+//     await getData(5);
+//     await getData(6);
+// }
+
+// getAllData();
+
+// : Immediately Invoked Function Expression
+// IIFE is a function that is called immediately as soon as it is defined,
+
+
+(async function (){
+    await getData(1);
+    await getData(2);
+    await getData(3);
+    await getData(4);
+    await getData(5);
+    await getData(6);
+})();
+
+//we can use only once
